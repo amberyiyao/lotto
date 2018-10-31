d721875e09acc5f7b960cc43b9733196808901b6
Amber Zhou <zhou0160@algonquinlive.com>
2018-10-31 17:38:27 -0400

    add

--- js/main.js
+++ js/main.js
@@ -1,72 +1,113 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    document.getElementById("btnSend").addEventListener("click", getData);
-    document.getElementById("btnBack").addEventListener("click", getData);
-}
+
+    document.getElementById("btnBack").addEventListener("click", function () {
+        document.getElementById("home").classList.toggle("display");
+        document.getElementById("list").classList.toggle("display");
+
+    });
+
+    document.getElementById("btnSend").addEventListener("click", function () {
+        document.getElementById("home").classList.toggle("display");
+        document.getElementById("list").classList.toggle("display");
+
+        var x = document.getElementById("digits").value;
+        console.log(x);
+        var y = document.getElementById("max").value;
+        console.log(y);
 
 
-function getData() {
+        let formData = new FormData();
 
-    let n = document.getElementById("digits").value;
-    console.log(n);
-    let c = document.getElementById("max").value;
-
-    function getRandomInteger(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max + 1); // max is exclusive
-        return Math.floor(Math.random() * (max + 1 - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-
-    let url = "https://davidst.edumedia.ca/mad9014/nums.php?digits=6&max=42";
-
-    let formData = new FormData();
-
-    formData.append("digits", n);
-    formData.append("max", c);
-
-    let customSettings = {
-        mode: "cors",
-        method: "POST",
-        body: formData
-    };
-
-    let request = new Request(url, customSettings);
+        let url = "https://davidst.edumedia.ca/mad9014/nums.php";
 
 
-    fetch(request)
-        .then(function (response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(
-            function (data) {
-                //
+        formData.append("digits", x);
+        formData.append("max", y);
 
-                console.log(c);
+        let customSettings = {
+            mode: "cors",
+            method: "POST",
+            body: formData
+        };
 
-
-                for (let i = 0; i < document.n; i++) {
-                    data.numbers.push(getRandomInteger(1, c))
-
-                }
+        let request = new Request(url, customSettings);
 
 
-                console.log(data.numbers);
-                let ul = document.querySelector(".num_list");
-                ul.innerHTML = ""; //clear out the old list
-                for (let item in data.numbers) {
-                    if (item < n) {
+        fetch(request)
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(
+                function (data) {
+                    //
+
+                    console.log(data.numbers);
+
+                    let ul = document.querySelector(".num_list");
+                    ul.innerHTML = ""; //clear out the old list
+                    for (let item in data.numbers) {
+
                         console.log(item + ": " + data.numbers[item]);
+
                         let li = document.createElement("li");
                         li.innerHTML = data.numbers[item];
                         ul.appendChild(li);
-                        console.log(data);
+
                     }
                 }
-            }
-        )
-        .catch(function (error) {
-            alert(error);
-        });
+            )
+            .catch(function (error) {
+                alert(error);
+            });
+
+    });
+
 }
+
+
+//
+//    let ul = document.querySelector(".num_list");
+//    ul.innerHTML = ""; //clear out the old list
+//    for (let item in numbers) {
+//
+////        console.log(item + ": " + numbers[item]);
+//        let li = document.createElement("li");
+//        li.innerHTML = numbers[item];
+//        ul.appendChild(li);
+
+
+
+
+
+
+//
+//document.getElementById("btnSend").addEventListener("click", function () {
+//
+//    var digits = document.getElementById("digits").value;
+//    console.log(digits);
+//    var y = document.getElementById("range").value;
+//    console.log(y);
+//
+//    function getRandomInteger(min, max) {
+//        min = Math.ceil(min); // min is inclusive
+//        max = Math.floor(max + 1); // max is exclusive so we add one
+//        return Math.floor(Math.random() * (max - min)) + min;
+//    }
+//
+//    let b = y;
+//    let a = getRandomInteger(1, b);
+//    console.log(a);
+//
+//    let lotto = [];
+//
+//    for (let i = 0; i < digits; i++) {
+//
+//        lotto.push(getRandomInteger(1, b));
+//
+//    }
+//
+//    console.log(lotto);
+//})
